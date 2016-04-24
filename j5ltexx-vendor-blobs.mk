b401c6a6a54393f6e58f83e52a72ab5f8ed5e94d
Ganesh Varma <ganeshvarmai4u@gmail.com>
2016-04-24 18:53:09 +0530

    Update j5ltexx-vendor-blobs.mk

--- j5ltexx-vendor-blobs.mk
+++ j5ltexx-vendor-blobs.mk
@@ -24,3 +24,4 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/j5ltexx/proprietary/lib/libaudio-ril.so:system/lib/libaudio-ril.so \
     vendor/samsung/j5ltexx/proprietary/lib/libsec-ril-dsds.so:system/lib/libsec-ril-dsds.so \
     vendor/samsung/j5ltexx/proprietary/lib/hw/audio.primary.msm8916.so:system/lib/hw/audio.primary.msm8916.so \
+
