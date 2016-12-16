a61be4b11053800f36b281838fe2c42f9518e3c5
Ganesh Varma <ganeshvarmai4u@gmail.com>
2016-12-17 00:26:13 +0530

    more Radio services

--- j5ltexx-vendor-blobs.mk
+++ j5ltexx-vendor-blobs.mk
@@ -18,6 +18,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/j5ltexx/proprietary/bin/rild:system/bin/rild \
     vendor/samsung/j5ltexx/proprietary/bin/rmnetcli:system/bin/rmnetcli \
     vendor/samsung/j5ltexx/proprietary/vendor/lib/libril-qcril-hook-oem.so:system/vendor/lib/libril-qcril-hook-oem.so \
+	vendor/samsung/j5ltexx/proprietary/vendor/bin/RIDLClient.exe:system/vendor/bin/RIDLClient.exe \
     vendor/samsung/j5ltexx/proprietary/lib/librmnetctl.so:system/lib/librmnetctl.so \
     vendor/samsung/j5ltexx/proprietary/lib/libril.so:system/lib/libril.so \
     vendor/samsung/j5ltexx/proprietary/lib/libsecnativefeature.so:system/lib/libsecnativefeature.so \
